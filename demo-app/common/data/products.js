385fcbd3536391010568ad5e00e9467535392f4a
Brian Cavalier <brian@hovercraftstudios.com>
2013-09-10 19:22:34 -0400

    Update product data. Tweak slides, update pdf version

--- demo-app/common/data/products.js
+++ demo-app/common/data/products.js
@@ -1,10 +1,10 @@
 define([
-	{ "id": 1, "name": "Chess", "description": "Record your crashes in HD", "price": 5 },
-	{ "id": 2, "name": "Monopoly", "description": "Announce loudly that you're about to crash", "price": 10 },
-	{ "id": 3, "name": "Scrabble", "description": "Bling for your bot", "price": 10 },
-	{ "id": 4, "name": "Go", "description": "Fun for all ages, until someone gets poked in the eye", "price": 20 },
-	{ "id": 5, "name": "IPA", "description": "You need it", "price": 200 },
-	{ "id": 6, "name": "Vanilla Porter", "description": "Frickin' laser beams, what else do you need??", "price": 200 },
-	{ "id": 7, "name": "Stout", "description": "Warning: May be incompatible with Machine that goes PING", "price": 500 },
-	{ "id": 8, "name": "Dunkel", "description": "Giorgio (the History Channel guy) would be proud", "price": 1000 }
+	{ "id": 1, "name": "Complete Book of Chess Strategy", "description": "Study it and become a master!", "price": 20 },
+	{ "id": 2, "name": "How to Cheat at Chess", "description": "Who cares, I just want to win", "price": 45 },
+	{ "id": 3, "name": "Official Scrabble Dictionary", "description": "Win at scrabble, or just impress your friends with truly obscure words", "price": 10 },
+	{ "id": 4, "name": "Everything I Know About Business I Learned From Monopoly", "description": "Yes, this is a real book", "price": 5 },
+	{ "id": 5, "name": "Mikeller Brunch Weasel Stout", "description": "How can you pass up a beer named Brunch Weasel?", "price": 7 },
+	{ "id": 6, "name": "Erie Railbender", "description": "A Pennsylvania favorite", "price": 5 },
+	{ "id": 7, "name": "Guinness Foreign Extra", "description": "The stuff that actually tastes like Guinness here in the US", "price": 4 },
+	{ "id": 8, "name": "Dogfish Head 90 Minute IPA", "description": "One of my favorites, so it makes the list", "price": 5 }
 ]);
